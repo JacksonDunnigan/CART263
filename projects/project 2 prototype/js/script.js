89c37f296b11e9d134c04d3c1c7625f96925b3ef
Jackson Dunnigan <43177490+JacksonDunnigan@users.noreply.github.com>
2022-03-17 14:34:43 -0400

    P2: Added the ability to pick up objects

--- projects/project 2 prototype/js/script.js
+++ projects/project 2 prototype/js/script.js
@@ -7,21 +7,56 @@ window.addEventListener('load', function() {
       Render = Matter.Render,
       Runner = Matter.Runner,
       Bodies = Matter.Bodies,
-      Composite = Matter.Composite;
+      Composite = Matter.Composite,
+      Events = Matter.Events,
+      World = Matter.World,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse = Matter.Mouse;
 
   // create an engine
   var engine = Engine.create();
+      world = engine.world;
 
   // create a renderer
   var render = Render.create({
       element: document.body,
-      engine: engine
+      engine: engine,
+      options: {
+           width: 800,
+           height: 600,
+           showAngleIndicator: true,
+       }
   });
 
   // create two boxes and a ground
   var boxA = Bodies.rectangle(400, 200, 80, 80);
   var boxB = Bodies.rectangle(450, 50, 80, 80);
   var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+
+  // add mouse control
+  // Events.on(engine, 'afterUpdate', function() {
+  //   if (mouseConstraint.mouse.button === -1) {
+  //           rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
+  //           World.add(engine.world, rock);
+  //           elastic.bodyB = rock;
+  //       }
+  //   });
+  var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+          mouse: mouse,
+          constraint: {
+              stiffness: 0.2,
+              render: {
+                  visible: false
+              }
+          }
+      });
+
+  World.add(world, mouseConstraint);
+
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
 
   // add all of the bodies to the world
   Composite.add(engine.world, [boxA, boxB, ground]);
